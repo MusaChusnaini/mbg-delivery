a1a9d356d414da69629310730668d07d92d74ca6
MusaChusnaini <musachusnaini@gmail.com>
2026-04-24 21:14:21 +0700

    GM10-Test

--- MUSA/models/player_car.gd
+++ MUSA/models/player_car.gd
@@ -3,6 +3,8 @@ extends VehicleBody3D
 const MAX_STEER = 0.8
 const ENGINE_POWER = 140
 
+# Check JIRA integration
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
